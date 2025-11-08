5c05eb79838b37af90b23e7f8ba037502584228c
Pedro Witzel <pedro.witzel@gmail.com>
2025-11-08 23:24:30 +0100

    feat: autosave formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,27 @@
 -- Autoformat
 return {
   'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
+  event = { 'BufReadPre', 'BufNewFile', 'BufWritePre' },
   cmd = { 'ConformInfo' },
   keys = {
     {
       '<leader>f',
       function()
-        require('conform').format { async = true, lsp_format = 'fallback' }
+        require('conform').format {
+          async = true,
+          lsp_format = 'fallback',
+        }
       end,
       mode = '',
       desc = '[F]ormat buffer',
     },
   },
+  config = function()
+    require('conform').setup {
+      format_on_save = {
+        lsp_format = 'fallback',
+        timeout_ms = 500,
+      },
+    }
+  end,
 }
