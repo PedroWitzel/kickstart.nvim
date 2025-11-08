f5792368f3760675c69977340809aeb22c630263
Pedro Witzel <pedro.witzel@gmail.com>
2025-11-08 22:48:38 +0100

    feat: moving conform, todo and TS

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+-- Autoformat
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+}
